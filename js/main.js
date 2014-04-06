d32f6b5c80f66c380df368191cc2e0f0d9fea947
r18 <twkgr424@yahoo.co.jp>
2014-04-06 18:38:26 +0900

    Add setStone method

--- js/main.js
+++ js/main.js
@@ -13,18 +13,25 @@ function main() {
 }
 
 function init() {
-  ctx.lineWidth = 1
+  ctx.lineWidth = 1;
   ctx.beginPath();
   for(var y = 0; y<= Y_LINES;++y){
-   ctx.moveTo(X_OFFSET,y*UNIT+Y_OFFSET);
-   ctx.lineTo(X_OFFSET+X_LINES*UNIT,y*UNIT+Y_OFFSET);
+    ctx.moveTo(X_OFFSET,y*UNIT+Y_OFFSET);
+    ctx.lineTo(X_OFFSET+X_LINES*UNIT,y*UNIT+Y_OFFSET);
   }
 
   for(var x = 0; x<= X_LINES;++x){
-   ctx.moveTo(X_OFFSET+x*UNIT,Y_OFFSET);
-   ctx.lineTo(X_OFFSET+x*UNIT,Y_LINES*UNIT+Y_OFFSET);
+    ctx.moveTo(X_OFFSET+x*UNIT,Y_OFFSET);
+    ctx.lineTo(X_OFFSET+x*UNIT,Y_LINES*UNIT+Y_OFFSET);
   }
 
   ctx.closePath();
   ctx.stroke();
+}
+
+function setStone(x,y,isWhite) {
+  ctx.beginPath();
+  ctx.arc(x*UNIT+X_OFFSET,y*UNIT+Y_OFFSET,UNIT/3,0,Math.PI*2,false);
+  ctx.closePath();
+  ctx.fill();
 }
