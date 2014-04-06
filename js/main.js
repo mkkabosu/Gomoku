fdcda08656b33debcc170f35d2c4c80cff0a7f40
r18 <twkgr424@yahoo.co.jp>
2014-04-06 18:26:24 +0900

    Add lattice

--- js/main.js
+++ js/main.js
@@ -2,9 +2,29 @@ function main() {
   console.log("start"); 
   cvs = document.getElementById("cvs");
   ctx = cvs.getContext('2d');
-    init();
+  X_LINES = 4;
+  Y_LINES = 4;
+  X_OFFSET = 10;
+  Y_OFFSET = 10;
+  UNIT = 24;
+  cvs.width = X_LINES * UNIT + X_OFFSET+3; 
+  cvs.height = Y_LINES * UNIT + Y_OFFSET+3; 
+  init();
 }
 
 function init() {
-  ctx.fillRect(0,0,100,100);
+  ctx.lineWidth = 1
+  ctx.beginPath();
+  for(var y = 0; y<= Y_LINES;++y){
+   ctx.moveTo(X_OFFSET,y*UNIT+Y_OFFSET);
+   ctx.lineTo(X_OFFSET+X_LINES*UNIT,y*UNIT+Y_OFFSET);
+  }
+
+  for(var x = 0; x<= X_LINES;++x){
+   ctx.moveTo(X_OFFSET+x*UNIT,Y_OFFSET);
+   ctx.lineTo(X_OFFSET+x*UNIT,Y_LINES*UNIT+Y_OFFSET);
+  }
+
+  ctx.closePath();
+  ctx.stroke();
 }
