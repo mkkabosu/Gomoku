3dd6f0c7e7ce853bcb3d7ee8448262eb7b8c80b6
hrl7 <hrlclb@gmail.com>
2014-04-06 17:55:27 +0900

    Enable to show Black rectangle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function main() {
   console.log("start"); 
+  cvs = document.getElementById("cvs");
+  ctx = cvs.getContext('2d');
+    init();
+}
+
+function init() {
+  ctx.fillRect(0,0,100,100);
 }
